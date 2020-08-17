2b83363313b695bb71e5ee62b059739ad465899d
Flora Petersen <floramarina@comcast.net>
2020-08-17 23:47:49 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,13 +1,14 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-
-  def characters
-    self.characters.collect do |character|
-      "#{character.name}"
-    end
-  end
+  has_many :shows, through: :characters
 
   def full_name
     "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,6 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :catchphrase
       t.integer :show_id
       t.integer :actor_id
     end
