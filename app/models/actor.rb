1880a086916dbd81583570262733f3c2fb03ca2a
Flora Petersen <floramarina@comcast.net>
2020-08-17 00:06:49 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def characters
+    self.characters.collect do |character|
+      "#{character.name}"
+    end
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
